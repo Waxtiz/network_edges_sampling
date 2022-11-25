ea11b2874eb5fa1b017dd60ffdebe61b2db30344
t_leclerc <t_leclerc@hotmail.fr>
2022-11-25 10:22:56 +0100

    nb_nodes option

--- main.R
+++ main.R
@@ -9,6 +9,7 @@
 library(sfnetworks) # https://github.com/luukvdmeer/sfnetworks
 library(sf)
 library(dplyr)
+library(progress)
 
 
 
@@ -62,52 +63,52 @@ random_path <- function(net,                              # Network
 
 
 
-
-#### Function to randomly sample a number of paths between a min and max length
-get_me_sample_path <- function(net,                       # Network
-                               number = 1,                # number of desired section
-                               min_length = 2000,         # minimal length of section (using unit of crs)
-                               max_length = 5000          # maximal length of section (using unit of crs)
-                              ) {
-
-# just progress bar for long statment
-  pb <- progress_bar$new(format = "(:spin) [:bar] :percent [Elapsed time: :elapsed || Estimated time: :eta]",
-                         total = number,
-                         clear = FALSE,
-                         show_after = 2)
-
-  i <- 0                                                  # count good paths
-  while (i != number) {                                   # keep searching until the number of good paths is reached
-      while(TRUE) {                                       # just a little hack to continue until counter order
-        random_nodes <- sample_nodes(net)                 # select random nodes in network
-        path <- random_path(net, random_nodes) %>%        # calculate the shortest path into nodes
-          select(geometry) %>%                            # keep only geom
-          summarise()                                     # summarise multiline into one single line
-
-        current_length <- sum(st_length(path)) %>%        # calculate the length of the path
-          as.integer()
-
-        ifelse(current_length > min_length &
-                 current_length < max_length,             # checks if the length is between min/max
-               ifelse(i == 0,                             # If yes, check if this is the first path
-                      break,                              # If yes, break the loop
-                      ifelse(sum(st_intersects(path, paths_df, sparse = F)) == 0, # check if there is no intersection
-                             break,                       # if there is no intersection, break the loop
-                             next)),                      # if there is intersection, try again
-              next)                                       # if the length is not between min/max, try again
-        }
-    i <- i+1                                              # if all checks are ok, increases the counter by 1
-    path <- mutate(path, id_path = as.integer(i))         # add id number of the path
-
-    if (i == 1) {                                         # check if this is the first path
-      paths_df <- path                                    # if yes, creat dataframe of paths with first path
-    } else if (i > 1) {
-      paths_df <- rbind(paths_df, path)                   # Then, add row of new path into exsiting dataframe of paths
-    }
-    pb$tick()                                             # increases the progress bar statment
-  }
-  return(paths_df)
-}
+#
+# #### Function to randomly sample a number of paths between a min and max length
+# get_me_sample_path <- function(net,                       # Network
+#                                number = 1,                # number of desired section
+#                                min_length = 2000,         # minimal length of section (using unit of crs)
+#                                max_length = 5000,         # maximal length of section (using unit of crs)
+#                                n_nodes = 2 ) {
+#
+# # just progress bar for long statment
+#   pb <- progress_bar$new(format = "(:spin) [:bar] :percent [Elapsed time: :elapsed || Estimated time: :eta]",
+#                          total = number,
+#                          clear = FALSE,
+#                          show_after = 2)
+#
+#   i <- 0                                                  # count good paths
+#   while (i != number) {                                   # keep searching until the number of good paths is reached
+#       while(TRUE) {                                       # just a little hack to continue until counter order
+#         random_nodes <- sample_nodes(net, n_nodes)        # select random nodes in network
+#         path <- random_path(net, random_nodes) %>%        # calculate the shortest path into nodes
+#           select(geometry) %>%                            # keep only geom
+#           summarise()                                     # summarise multiline into one single line
+#
+#         current_length <- sum(st_length(path)) %>%        # calculate the length of the path
+#           as.integer()
+#
+#         ifelse(current_length > min_length &
+#                  current_length < max_length,             # checks if the length is between min/max
+#                ifelse(i == 0,                             # If yes, check if this is the first path
+#                       break,                              # If yes, break the loop
+#                       ifelse(sum(st_intersects(path, paths_df, sparse = F)) == 0, # check if there is no intersection
+#                              break,                       # if there is no intersection, break the loop
+#                              next)),                      # if there is intersection, try again
+#               next)                                       # if the length is not between min/max, try again
+#         }
+#     i <- i+1                                              # if all checks are ok, increases the counter by 1
+#     path <- mutate(path, id_path = as.integer(i))         # add id number of the path
+#
+#     if (i == 1) {                                         # check if this is the first path
+#       paths_df <- path                                    # if yes, creat dataframe of paths with first path
+#     } else if (i > 1) {
+#       paths_df <- rbind(paths_df, path)                   # Then, add row of new path into exsiting dataframe of paths
+#     }
+#     pb$tick()                                             # increases the progress bar statment
+#   }
+#   return(paths_df)
+# }
 
 
 
@@ -116,7 +117,8 @@ get_me_sample_path <- function(net,                       # Network
                                number = 1,                # number of desired section
                                min_length = 2000,         # minimal length of section (using unit of crs)
                                max_length = 5000,         # maximal length of section (using unit of crs)
-                               overlap = FALSE) {         # can the paths overlap? (TRUE/FALSE)
+                               overlap = FALSE,           # can the paths overlap? (TRUE/FALSE)
+                               n_nodes = 2) {
 
 
   # just progress bar for long statment
@@ -128,8 +130,11 @@ get_me_sample_path <- function(net,                       # Network
   i <- 0                                                  # count good paths
   while (i != number) {                                   # keep searching until the number of good paths is reached
       while(TRUE) {                                       # just a little hack to continue until counter order
-        random_nodes <- sample_nodes(net)                 # select random nodes in network
-        path <- random_path(net, random_nodes) %>%        # calculate the shortest path into nodes
+        random_nodes <- sample_nodes(net, n_nodes)        # select random nodes in network
+
+        path <- suppressWarnings(                         # stop "Couldn't reach some vertices." warning
+          random_path(net, random_nodes)                  # calculate the shortest path into nodes
+                                ) %>%
           select(geometry) %>%                            # keep only geom
           summarise()                                     # summarise multiline into one single line
 
@@ -194,7 +199,7 @@ net <- ign_road %>%
 
 
 
-path <- get_me_sample_path(net, 30, 25000, 35000, overlap = F)
+path <- get_me_sample_path(net, 5, 1000, 10000, overlap = F, 2)
 
 
 # Visualise result
@@ -207,4 +212,4 @@ ggplot() +
   labs(color = "Paths")
 
 
-ifelse(isTRUE(test), "yes, is true", "nop")
+
